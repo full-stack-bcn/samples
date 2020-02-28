06f37258d15ef844dc63b43b40761c5267d66387
Pau Fernández <pau.fernandez@upc.edu>
2020-02-28 14:47:30 +0100

    New sample: function bind

---
+++ javascript/functions/function-bind.js
@@ -0,0 +1,18 @@
+/* 
+  The bind method sets the value of 'this' within the function
+*/
+
+function compute() {
+  console.log(`${this.a} + ${this.b} =`, this.a + this.b);
+}
+
+// If we call it directly, the 'this' object won't have 'a' and 'b'
+compute();
+
+// If we bind the function to an object with those fields, it will work correctly
+let obj = { a: 2, b: 3 };
+const boundCompute = compute.bind(obj);
+boundCompute();
+
+// We can also do it in one step
+compute.bind({ a: 7, b: 11 })();
