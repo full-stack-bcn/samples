44c1ef41c2e2a897ada84f6afc645b3f1c80e511
Pau Fernández <pau.fernandez@upc.edu>
2020-02-28 12:44:47 +0100

    Rewrote sample about destructuring

--- javascript/objects/property-desctructuring.js
+++ javascript/objects/property-desctructuring.js
@@ -1,12 +1,35 @@
-let obj = {
-  x: 1,
-  y: "hi",
-  z: true
+/*
+   Destructuring
+*/
+let obj1 = {
+  a: 1,
+  b: "hi"
 };
 
-const { x, y, z } = obj;
-console.log(`x = ${x}, y = ${y}, z = ${z}`);
-
-const { x: a, y: b, ...rest } = obj;
+let { a } = obj1;
+const { b } = obj1;
 console.log(`a = ${a}, b = ${b}`);
-console.log(rest);
+
+/* 
+  2. Destructuring of the rest of fields
+*/
+let obj2 = {
+  x: 0.01,
+  y: 0.2,
+  z: 12.7,
+  t: 10.5
+};
+
+let { x, y, ...rest } = obj2;
+console.log(`x = ${x}, y = ${y} (and z = ${rest.z}, t = ${rest.t})`);
+
+/* 
+  3. Renaming some fields 
+*/
+let obj3 = {
+  name: "James",
+  age: 27
+};
+
+let { name: user, age: years } = obj3;
+console.log(`user = ${user}, years = ${years}`);
