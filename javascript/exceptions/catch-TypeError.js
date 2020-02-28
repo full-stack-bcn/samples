1762a2e24b9aeb81216f91a2e69584bd4cda84b8
Pau Fernández <pau.fernandez@upc.edu>
2020-02-28 12:31:44 +0100

    Reformatted sample

--- javascript/exceptions/catch-TypeError.js
+++ javascript/exceptions/catch-TypeError.js
@@ -1,14 +1,16 @@
-function second(obj) {
+const second = (obj) => {
   obj.clearlyNonExistentMethod();
 }
-function first() {
+
+const first = () => {
   let x = { a: 1, b: "2", c: [3] };
   second(x);
 }
+
 try {
   first();
 } catch (e) {
-  console.log("Error Type: ", e.name);
-  console.log("Error Message: ", e.message);
-  console.log("Error stack: ", typeof e.stack);
+  console.log("Error Type:       ", e.name);
+  console.log("Error Message:    ", e.message);
+  console.log("Error stack type: ", typeof e.stack);
 }
