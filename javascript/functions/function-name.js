a19ddb1c27110c27ce86136bb36ab26abf9bb201
Pau Fernández <pau.fernandez@upc.edu>
2020-02-28 14:40:11 +0100

    Improved the function name sample

--- javascript/functions/function-name.js
+++ javascript/functions/function-name.js
@@ -1,29 +1,23 @@
-// Since a function is an object, you can ask for its name
-
-function f1() {
-  console.log("You called f1");
-}
-
-const f2 = function() {
-  console.log("You called f2");
-};
-
-const f3 = f2;
-
-const f4 = [
-  function() {
-    console.log("You called f4");
-  }
-];
+/*
+  Since a function is an object, you can ask for its name
+*/
 
 // A 'named' function has an obvious name
+function f1() {}
 console.log(f1.name);
 
 // An anonymous function also gets a name (it is cleverly deduced by Javascript)
+const f2 = function() {};
 console.log(f2.name);
 
 // What about a copy of f2?
+const f3 = f2;
 console.log(f3.name);
 
-// The name is emtpy in this case (Javascript couldn't deduce it)
+// The name is emtpy in this case (Javascript won't deduce it)
+const f4 = [function() {}];
 console.log(`"${f4[0].name}"`);
+
+// You can name a function even if you store it somewhere else
+const f5 = function abc() {};
+console.log(f5.name);
