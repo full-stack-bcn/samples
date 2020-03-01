4c16507ec67e464bc87af88f85867aba391e4e7b
Pau Fernández <pau.fernandez@upc.edu>
2020-03-01 15:43:56 +0100

    Array spread sample

---
+++ javascript/arrays/array-spread.js
@@ -0,0 +1,16 @@
+// The spread operator can be used with arrays to
+// "spread" all elements in an array literal
+let source = [3, 4];
+let dest = [1, 2, ...source, 5];
+console.log(dest);
+
+// We can even use the same array twice
+let source2 = [...source, ...source];
+console.log(source2);
+
+// Array spread just asks for the object to be an iterable
+let text = "lorem ipsum";
+let a = [...text];
+console.log(a);
+
+
