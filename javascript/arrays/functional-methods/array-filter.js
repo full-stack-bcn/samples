7c9dddf6043aaf7cebf47d2bf7f4fc19cee37d14
Pau Fernández <pau.fernandez@upc.edu>
2020-03-01 16:47:00 +0100

    Polished array-filter sample

--- javascript/arrays/functional-methods/array-filter.js
+++ javascript/arrays/functional-methods/array-filter.js
@@ -1,8 +1,10 @@
+// 'filter' produces a new sequence with the elements in which the passed
+// function returns true.
 let students = [
   { name: "Peter", mark: 6.7 },
   { name: "Paul", mark: 3.9 },
   { name: "Mary", mark: 9.2 }
 ];
 
-let passed = students.filter(student => student.mark > 5.0);
-passed.forEach(s => console.log(s.name));
+let passed = students.filter(s => s.mark > 5.0);
+passed.forEach(s => console.log(`${s.name}: ${s.mark}`));
