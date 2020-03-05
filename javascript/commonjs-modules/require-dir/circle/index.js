f262e76cc7a73906123ba9cc2da71e793b4aa917
Pau Fernández <pau.fernandez@upc.edu>
2020-03-05 14:42:30 +0100

    Changes the way of exporting area and perimiter

--- javascript/commonjs-modules/require-dir/circle/index.js
+++ javascript/commonjs-modules/require-dir/circle/index.js
@@ -1,9 +1,13 @@
+// The main file in a package without 'package.json' is index.js
+
 const perimeter = radius => 2 * Math.PI * radius;
 const area = radius => Math.PI * radius ** 2;
 
-// To make functions visible outside of the module
-// we add them to the 'exports' object
-exports.area = area;
-exports.perimeter = perimeter;
+// We overwrite the exports object and thus we need to do it in the module
+// object
+module.exports = {
+  area,
+  perimeter
+};
 
-console.log('circle/index.js loaded!');
+console.log("circle/index.js directory module loaded!");
