1b94acd148e30b49d19ab5bdf579832f57284810
Pau Fernández <pau.fernandez@upc.edu>
2020-03-05 14:29:14 +0100

    Added "require-dir" sample

---
+++ javascript/commonjs-modules/require-dir/circle/index.js
@@ -0,0 +1,9 @@
+const perimeter = radius => 2 * Math.PI * radius;
+const area = radius => Math.PI * radius ** 2;
+
+// To make functions visible outside of the module
+// we add them to the 'exports' object
+exports.area = area;
+exports.perimeter = perimeter;
+
+console.log('circle/index.js loaded!');
---
+++ javascript/commonjs-modules/require-dir/main.js
@@ -0,0 +1,5 @@
+
+const { area, perimeter } = require('./circle');
+
+console.log(area(2.5));
+console.log(perimeter(1.8));
