1ac038786916c45cc7eba9bfcc988be589cc5614
Pau Fernández <pau.fernandez@upc.edu>
2020-03-12 13:19:59 +0100

    Improved sample about the type switch

--- javascript/concepts/classes/es6/type-switch.js
+++ javascript/concepts/classes/es6/type-switch.js
@@ -1,4 +1,5 @@
 
+// We have a base class Command, and a Derived class SpecialCommand
 class Command { }
 
 class SpecialCommand extends Command {
@@ -8,12 +9,14 @@ class SpecialCommand extends Command {
   }
 }
 
+// We create two objects, one of each class
 let c1 = new Command()
 let c2 = new SpecialCommand('x67$%')
 
+// A function that prints different things depending on the class of the 'c'
 const showClass_v1 = (c) => {
   if (c instanceof SpecialCommand) {
-    console.log(`--> InsertCommand(${c.data})`);
+    console.log(`--> SpecialCommand(${c.data})`);
   } else if (c instanceof Command) {
     console.log("--> Command");
   } else {
@@ -21,22 +24,25 @@ const showClass_v1 = (c) => {
   }
 }
 
+// The same function implemented differently
 const showClass_v2 = (c) => {
   switch (c.constructor) {
     case SpecialCommand:
-      console.log(`--> InsertCommand(${c.data})`);
+      console.log(`--> SpecialCommand(${c.data})`);
       break;
     case Command:
       console.log("--> Command");
       break;
     default:
-      console.log("Nothing...");
+      console.log("--> Unknown");
   }
 }
 
+// Test 1
 showClass_v1(c1)
 showClass_v1(c2)
 
+// Test 2
 showClass_v2(c1)
 showClass_v2(c2)
 
