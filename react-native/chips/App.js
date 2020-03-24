d80438a6659b8ae13d335661acef634b4a253103
Pau Fernández <pau.fernandez@upc.edu>
2020-03-24 15:42:25 +0100

    Added React Native sample: Chips

---
+++ react-native/chips/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Chip = ({ id, first, children }) => {
+  const { center, chip } = styles;
+  return (
+    <View key={id} style={[chip, center, { marginLeft: first ? 0 : 5 }]}>
+      {children}
+    </View>
+  );
+};
+
+export default function App() {
+  const { page, center, chips, chip } = styles;
+  const tags = ["jazz", "hip-hop", "rock", "classical"];
+  return (
+    <View style={[page, center]}>
+      <View style={[chips, center]}>
+        {tags.map((tag, i) => (
+          <Chip id={tag} first={i === 0}>
+            <Text>{tag}</Text>
+          </Chip>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  center: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  page: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  chips: {
+    flexDirection: "row"
+  },
+  chip: {
+    flexDirection: "row",
+    paddingLeft: 15,
+    paddingRight: 15,
+    height: 35,
+    backgroundColor: "#eca",
+    borderRadius: 35
+  }
+});
