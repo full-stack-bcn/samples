2da154e414675df72694de94336e9c2a48302700
Pau Fernández <pau.fernandez@upc.edu>
2020-04-23 11:37:24 +0200

    Polished RN Custom Fonts sample

--- react-native/custom-fonts/App.js
+++ react-native/custom-fonts/App.js
@@ -1,49 +1,51 @@
 import * as Font from "expo-font";
-import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 
-export default class App extends Component {
-  state = {
-    fontLoaded: false
-  };
+const App = () => {
+  const [fontLoaded, setFontLoaded] = useState(false);
 
-  // To load the font, we call Font.loadAsync (and await on it)
-  // then set the fontLoaded state to true
-  async componentDidMount() {
-    await Font.loadAsync({
-      "Oswald-SemiBold": require("./assets/fonts/Oswald-SemiBold.ttf"),
-    });
-    this.setState({ fontLoaded: true });
-  }
-
-  render() {
-    const { page, text } = styles;
-    // In the component, we have to wait until fonts are loaded to use them
-    if (!this.state.fontLoaded) {
-      return (
-        <View style={page}>
-          <Text>Loading...</Text>
-        </View>
-      );
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        "Oswald-SemiBold": require("./assets/fonts/Oswald-SemiBold.ttf"),
+      })
+      setFontLoaded(true);
     }
+    loadFonts();
+  }, []);
+
+  // In the component, we have to wait until fonts are loaded to use them
+  if (!fontLoaded) {
     return (
-      <View style={page}>
-        <Text style={text}>This text is shown with a custom font!</Text>
+      <View style={styles.loading}>
+        <ActivityIndicator />
       </View>
-    );
+    );  
   }
-}
+  return (
+    <View style={styles.page}>
+      <Text style={styles.text}>This text is shown with a custom font!</Text>
+    </View>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
+  loading: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   page: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "stretch",
     backgroundColor: "#fff",
-    padding: 40
+    padding: 40,
   },
   text: {
-    fontFamily: 'Oswald-SemiBold',
+    fontFamily: "Oswald-SemiBold",
     fontSize: 40,
-  }
+  },
 });
