e562e0745bd3fba2f07b912c8ea596b06b958156
Pau Fernández <pau.fernandez@upc.edu>
2020-06-04 17:40:46 +0200

    Simplified further

--- fullstack/todos/frontend/src/api/index.js
+++ fullstack/todos/frontend/src/api/index.js
@@ -1,28 +1,21 @@
 const BASE_URL = "http://localhost:8080";
 
-const _call = async (path, options) => {
+const _apiCall = async (method, path, payload) => {
+  let options = { method, mode: "cors" };
+  if (payload) {
+    options.headers = { "Content-Type": "application/json" };
+    options.body = JSON.stringify(payload);
+  }
   const response = await fetch(`${BASE_URL}/api${path}`, options);
   const result = await response.json();
   return result;
 };
 
-const _mutate = async (method, path, payload) => {
-  let options = { method, mode: "cors" };
-  if (method === "PUT" || method === "POST") {
-    options = {
-      ...options,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(payload),
-    };
-  }
-  return _call(path, options);
-};
-
-export const loadTodos = () => _call("/todos");
-export const addTodo = (text) => _mutate("POST", "/todos", { text });
-export const updateTodo = (todo) => _mutate("PUT", `/todos/${todo.id}`, todo);
-export const deleteTodo = (id) => _mutate("DELETE", `/todos/${id}`);
-export const clearCompleted = () => _mutate("DELETE", "/todos");
+export const loadTodos = () => _apiCall("GET", "/todos");
+export const addTodo = (text) => _apiCall("POST", "/todos", { text });
+export const updateTodo = (todo) => _apiCall("PUT", `/todos/${todo.id}`, todo);
+export const deleteTodo = (id) => _apiCall("DELETE", `/todos/${id}`);
+export const clearCompleted = () => _apiCall("DELETE", "/todos");
 
 export default {
   loadTodos,
