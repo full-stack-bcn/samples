a1f9fc35826503d08b6bf78bcac0757aea081b88
Pau Fernández <pau.fernandez@upc.edu>
2020-06-16 12:40:58 +0200

    Added MongoDB sample

---
+++ databases/mongodb/mongodb-example.js
@@ -0,0 +1,18 @@
+
+const mongodb = require('mongodb');
+
+const url = "mongodb://admin:fullstack@localhost:27017";
+const client = new mongodb.MongoClient(url);
+
+async function main() {
+  await client.connect();
+  const db = client.db("test");
+  const result = await db.collection("foo").insertMany([
+    { a: 1, b: 2, c: "hola" },
+    { bla: "bla", bli: "bli" },
+  ]);
+  console.log(result);
+  client.close();
+}
+
+main();
