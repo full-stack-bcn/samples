6f3d341efc1f8e28b95a5dec5bc2d9d44e9dab22
Pau Fernández <pau.fernandez@upc.edu>
2022-05-12 09:17:51 +0200

    Añado links a los datos y arreglo script para Docker 2

---
+++ databases/mongodb-airbnb/csv/README.md
@@ -0,0 +1,8 @@
+
+Download CSV files from:
+
+http://data.insideairbnb.com/spain/catalonia/barcelona/2021-12-07/data/listings.csv.gz
+http://data.insideairbnb.com/spain/catalonia/barcelona/2021-12-07/data/calendar.csv.gz
+http://data.insideairbnb.com/spain/catalonia/barcelona/2021-12-07/data/reviews.csv.gz
+
+
--- databases/mongodb-airbnb/restore-db.sh
+++ databases/mongodb-airbnb/restore-db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 for collection in listings calendar reviews; do
-  docker exec -it mongodb-airbnb_db_1 mongoimport /csv/$collection.csv -u admin -p fullstack --type csv -d airbnb -c $collection --headerline --drop --authenticationDatabase admin
+  docker exec -it mongodb-airbnb-db-1 mongoimport /csv/$collection.csv -u admin -p fullstack --type csv -d airbnb -c $collection --headerline --drop --authenticationDatabase admin
 done
